72c6b3b98f9c1b6514624fd6c8ec87cf384e1284
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-05-02 23:43:44 +0900

    feat: added interface and mock class based on DI for dataset
    Dependency Injection can loses the connections between the classes

---
+++ keiba_prediction/core/interfaces.py
@@ -0,0 +1,8 @@
+import abc
+from core.types import horse
+
+
+class Ihorse_database(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def get_horse(self, id: int) -> horse:
+        return NotImplementedError()
---
+++ keiba_prediction/services/mock_database.py
@@ -0,0 +1,21 @@
+from core import interfaces
+from core.types import horse
+from typing import List
+
+
+class MockHorses(interfaces.Ihorse_database):
+    def __init__(self) -> None:
+        self.horses: List[horse] = [
+            horse(id=1, name="foo horse", weigth=23),
+            horse(id=2, name="great kun", weigth=49),
+            horse(id=3, name="ohhh ban", weigth=30),
+            horse(id=4, name="Kitasan Black", weigth=11),
+            horse(id=5, name="Sinboriru Dolf", weigth=200),
+        ]
+
+    def get_horse(self, id: int) -> horse:
+        for h in self.horses:
+            if h.id == id:
+                return h
+
+        return None
